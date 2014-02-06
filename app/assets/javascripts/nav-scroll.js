eeee321a1e3675c2f24ba98edf66ed48b7740924
Max Mearsheimer <mmearshe@gmail.com>
2014-02-06 14:55:06 -0600

    fixed nav so it has white background on scroll

--- app/assets/javascripts/nav-scroll.js
+++ app/assets/javascripts/nav-scroll.js
@@ -1,8 +1,10 @@
 $(document).scroll(function() {
   if($(window).scrollTop() > 75) {
           $(".nav").addClass("darklink");
+          $(".scrollback").css("opacity", "1");
       } else {
           $(".nav").removeClass("darklink");
+          $(".scrollback").css("opacity", "0");
       }
 });
 
