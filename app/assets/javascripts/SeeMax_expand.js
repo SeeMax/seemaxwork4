0ae326268b9af84fdb1314c20562ebab5cbb8931
Max Mearsheimer <mmearshe@gmail.com>
2014-02-07 14:50:43 -0600

    made Zearn in design active with loose outline. created general layout for projects.

--- app/assets/javascripts/SeeMax_expand.js
+++ app/assets/javascripts/SeeMax_expand.js
@@ -2,75 +2,65 @@
 
 // $(document).ready(function(){
 
-//   var urlRoot = location.toString().match(/training/) ? "/training" : "/programs";
+// // var urlRoot = location.toString().match(/training/) ? "/training" : "/programs";
 
-//   // content box open/closing
-//   //
-//   // if no boxes open, click to open it
-//   // if one box open, click it to close itself
-//   // if one box open, click another box
-//   $('.content-box').not('.disabled').click(function() {
-//     toggleCard($(this));
-//   });
 
 //   var currentCard;
 //   var toggleCard = function(card) {
-//     if ( card.is('#current') ) {
-//       $(".content-box#current").attr("id", 'current');
-//       card.removeAttr('id', 'current');
+//     if ( card.is('toggle') ) {
+//       $("client_box").attr('.active');
+//       card.removeClass('active');
 //       card.addClass('last-active');
 //       currentCard = null;
 //     } else {
-//       $(".content-box#current").removeAttr("id", 'current');
+//       $("client_box").removeClass('active');
 //       card.removeClass('last-active');
-//       card.attr("id","current");
+//       card.attr('active');
 //       currentCard = card[0];
 
-//       var wrapper = card.find(".paragraph-wrapper");
-//       if (wrapper.length) {
-//         $.get(card.attr("href"), function(r) {
-//           var content = $(r).find(".paragraph");
-//           content.find("img").on("load", function() {
-//             $('.isotope-container').isotope('reLayout');
-//           });
-//           wrapper.replaceWith(content);
-//           $('.isotope-container').isotope('reLayout');
-//         });
+//       // var wrapper = card.find(".paragraph-wrapper");
+//       // if (wrapper.length) {
+//       //   $.get(card.attr("href"), function(r) {
+//       //     var content = $(r).find(".paragraph");
+//       //     content.find("img").on("load", function() {
+//       //       $('.isotope-container').isotope('reLayout');
+//       //     });
+//       //     wrapper.replaceWith(content);
+//       //     $('.isotope-container').isotope('reLayout');
+//       //   });
 //       }
 //     }
 
-//     $('.isotope-container').isotope('reLayout');
+
+//     // if ( $(".content-box#current").length ) {
+//     //   setTimeout(function(){
+//     //     $('html, body').animate({
+//     //       scrollTop: $(".content-box#current").offset().top
+//     //     }, 1000);
+//     //   }, 500);
+//     // } else if ( $(".content-box.last-active").length ) {
+//     //   setTimeout(function(){
+//     //     $('html, body').animate({
+//     //       scrollTop: $(".content-box.last-active").offset().top
+//     //     }, 1000);
+//     //   }, 500);
+//     // }
 
 
-//     if ( $(".content-box#current").length ) {
-//       setTimeout(function(){
-//         $('html, body').animate({
-//           scrollTop: $(".content-box#current").offset().top
-//         }, 1000);
-//       }, 500);
-//     } else if ( $(".content-box.last-active").length ) {
-//       setTimeout(function(){
-//         $('html, body').animate({
-//           scrollTop: $(".content-box.last-active").offset().top
-//         }, 1000);
-//       }, 500);
-//     }
+//   //   if (currentCard) {
+//   //     History.pushState({url: card.attr("href")}, '', card.attr("href"));
+//   //   } else {
+//   //     History.pushState({url: ""}, '', urlRoot);
+//   //   }
+//   //   return false;
+//   // };
 
-
-//     if (currentCard) {
-//       History.pushState({url: card.attr("href")}, '', card.attr("href"));
-//     } else {
-//       History.pushState({url: ""}, '', urlRoot);
-//     }
-//     return false;
-//   };
-
-//   History.Adapter.bind(window,'statechange',function(){ // Note: We are using statechange instead of popstate
-//     var State = History.getState(); // Note: We are using History.getState() instead of event.state
-//     var card = $("div[href='" + State.data.url + "']");
-//     if (currentCard != card[0]) {
-//       toggleCard(card);
-//     }
-//   });
+//   // History.Adapter.bind(window,'statechange',function(){ // Note: We are using statechange instead of popstate
+//   //   var State = History.getState(); // Note: We are using History.getState() instead of event.state
+//   //   var card = $("div[href='" + State.data.url + "']");
+//   //   if (currentCard != card[0]) {
+//   //     toggleCard(card);
+//   //   }
+//   // });
 
 // });
