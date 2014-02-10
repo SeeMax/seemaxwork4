98da6d3b3bdddcb9d0b6dba03f7ceba392a73b0d
Max Mearsheimer <mmearshe@gmail.com>
2014-02-09 22:35:00 -0600

    fixed nav. home scrren and links to it. fixed titles copy and sub heads

--- app/assets/javascripts/nav-scroll.js
+++ app/assets/javascripts/nav-scroll.js
@@ -1,12 +1,15 @@
-$(document).scroll(function() {
-  if($(window).scrollTop() > 80) {
+$(document).ready(function() {
+  $(document).scroll(function() {
+    if($(window).scrollTop() > 80) {
           $(".nav").addClass("darklink");
           $(".scrollback").css("opacity", "1");
       } else {
           $(".nav").removeClass("darklink");
           $(".scrollback").css("opacity", "0");
       }
+  });
 });
+
 
 
 
