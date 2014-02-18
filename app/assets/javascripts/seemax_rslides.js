0e64e5c345464a5ecd9b2de5473250b64c70095e
Max Mearsheimer <mmearshe@gmail.com>
2014-02-17 23:03:28 -0600

    updated work files and images, added carousel, store now temp. still need to fix up height opening

---
+++ app/assets/javascripts/seemax_rslides.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $(function() {
+    $(".rslides").responsiveSlides({
+      auto: true,             // Boolean: Animate automatically, true or false
+      speed: 10,            // Integer: Speed of the transition, in milliseconds
+      timeout: 700,          // Integer: Time between slide transitions, in milliseconds
+    });
+  });
+});
