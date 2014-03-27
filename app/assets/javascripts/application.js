2363cf37796a48079cde9e6d25f74c3e78cb8f2f
Max Mearsheimer <mmearshe@gmail.com>
2014-03-27 13:00:00 -0500

    updated store and link. removed nightshift. renamed store gear.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,11 +2,13 @@
 //= require jquery_ujs
 //= require_tree .
 
-// $.getJSON("http://api.bigcartel.com/seemaxwork/products.json", function( array ) {
-//   $.each( array, function( key, item ) {
-//     img = item['images'][0]['url'];
-//     name = item['name'];
-//     $('.store-item').append('<img src="' + img + '">');
-//     $('.store-item').append(name);
-//   });
-// });
+$.getJSON("http://api.bigcartel.com/seemaxwork/products.json", function( array ) {
+  $.each( array, function( key, item ) {
+    img1 = item['images'][0]['url'];
+    name = item['name'];
+    description = item['description']
+    $('.lungs_1').append('<img src="' + img1 + '">');
+    $('.lungs_title').append(name);
+    $('.store_description').append(description);
+  });
+});
