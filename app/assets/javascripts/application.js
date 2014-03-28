752a1cca21890e31b377e14cc30ad7d895a0058b
Max Mearsheimer <mmearshe@gmail.com>
2014-03-28 10:26:37 -0500

    changed image to local for preview purposes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,10 +4,8 @@
 
 $.getJSON("http://api.bigcartel.com/seemaxwork/products.json", function( array ) {
   $.each( array, function( key, item ) {
-    img1 = item['images'][0]['url'];
     name = item['name'];
     description = item['description']
-    $('.lungs_1').append('<img src="' + img1 + '">');
     $('.lungs_title').append(name);
     $('.store_description').append(description);
   });
